ea818a8b3346a53dcf8ac527a26643311d19f5d4
Mohammad <mohdali0990@gmail.com>
2021-01-12 16:28:22 -0600

    updated

---
+++ src/main/java/BankingAtm/Thymeleaf/BankingAtmThymeleafApplication.java
@@ -0,0 +1,13 @@
+package BankingAtm.Thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAtmThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingAtmThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BankingAtm/Thymeleaf/BankingAtmThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package BankingAtm.Thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAtmThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
