6e4a5d50fc567a0e636339cd1ac3a825f70e6599
Mohammad <mohdali0990@gmail.com>
2021-01-22 16:47:04 -0600

    updated

--- src/main/java/BankingAtm/Thymeleaf/Controller/CheckingAccountController.java
+++ src/main/java/BankingAtm/Thymeleaf/Controller/CheckingAccountController.java
@@ -4,6 +4,7 @@ import BankingAtm.Thymeleaf.Model.CheckingAccount;
 import BankingAtm.Thymeleaf.Repository.CheckingAccountRepo;
 import BankingAtm.Thymeleaf.Service.CheckingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.Optional;
 
