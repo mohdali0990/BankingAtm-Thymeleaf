6c38338ad22307656907675f299b1de00fc268d6
Mohammad <mohdali0990@gmail.com>
2021-02-20 14:14:53 -0600

    modified

--- src/main/java/BankingAtm/Thymeleaf/Controller/SavingAccountController.java
+++ src/main/java/BankingAtm/Thymeleaf/Controller/SavingAccountController.java
@@ -5,13 +5,11 @@ import BankingAtm.Thymeleaf.Service.SavingAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@Validated 
 public class SavingAccountController {
 
     @Autowired
