734375eb054bee7fecef0b8620c7843f52ef422c
Mohammad <mohdali0990@gmail.com>
2021-03-18 16:32:17 -0500

    working on implementing spring security

--- src/main/java/BankingAtm/Thymeleaf/Security/SecurityConfig.java
+++ src/main/java/BankingAtm/Thymeleaf/Security/SecurityConfig.java
@@ -1,9 +1,17 @@
 package BankingAtm.Thymeleaf.Security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -25,4 +33,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //    }
 
 
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        List<UserDetails>users = new ArrayList<>();
+        users.add(User.withDefaultPasswordEncoder().username("john").password("cena").roles("USER").build());
+        return new InMemoryUserDetailsManager(users);
+    }
 }
